4a55f8e869b9bfc923bfa132152943fde35df732
Robiul <victorshakil9102@gmail.com>
2022-01-31 15:56:54 +0600

    some shortcut added

--- Movies.Client/Services/CRUDService.cs
+++ Movies.Client/Services/CRUDService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
@@ -26,7 +27,9 @@ namespace Movies.Client.Services
         {
             //await GetResource();
             //await GetResourceThroughHttpRequestMessage();
-            await CreateResource();
+            //await CreateResource();
+            //await UpdateResource();
+            await DeleteResource();
         }
 
         public async Task GetResource()
@@ -75,7 +78,7 @@ namespace Movies.Client.Services
             var serializedMovieToCreate = JsonSerializer.Serialize(movieToCreate);
 
             var request = new HttpRequestMessage(HttpMethod.Post, "api/movies");
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
 
             request.Content = new StringContent(serializedMovieToCreate);
             request.Content.Headers.ContentType.MediaType = "application/json";
@@ -85,9 +88,104 @@ namespace Movies.Client.Services
 
             var content = await response.Content.ReadAsStringAsync();
 
-            var movies = JsonSerializer.Deserialize<Movie>(content);
-
+            var movie = JsonSerializer.Deserialize<Movie>(content);
 
         }
+
+        public async Task UpdateResource()
+        {
+            var movieToUpdate = new MovieForUpdate()
+            {
+                Title = "Pulp Fiction",
+                Description = "The movie with Zed.",
+                DirectorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+                Genre = "Crime, Drama"
+            };
+
+            var serializedMovieToUpdate = JsonSerializer.Serialize(movieToUpdate);
+
+            var request = new HttpRequestMessage(HttpMethod.Put,
+                "api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b");
+            request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+            request.Content = new StringContent(serializedMovieToUpdate);
+            request.Content.Headers.ContentType.MediaType = "application/json";
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var updateMovie = JsonSerializer.Deserialize<Movie>(content);
+        }
+
+        public async Task DeleteResource()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete,
+                "api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b");
+            request.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+        }
+
+        private async Task PostResourceShortcut()
+        {
+            var movieToCreate = new MovieForCreation()
+            {
+                Title = "Reservoir Dogs",
+                Description = "After a simple jewelry heist goes terribly wrong, the " +
+                              "surviving criminals begin to suspect that one of them is a police informant.",
+                DirectorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+                Genre = "Crime, Drama"
+            };
+
+            var response = await _httpClient.PostAsync(
+                "api/movies", new StringContent(
+                    JsonSerializer.Serialize(movieToCreate), Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var movie = JsonSerializer.Deserialize<Movie>(content);
+
+        }
+
+        private async Task PutResourceShortcut()
+        {
+            var movieToUpdate = new MovieForUpdate()
+            {
+                Title = "Pulp Fiction",
+                Description = "The movie with Zed.",
+                DirectorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+                Genre = "Crime, Drama"
+            };
+
+            var response = await _httpClient.PutAsync(
+                "api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b",
+                new StringContent(
+                    JsonSerializer.Serialize(movieToUpdate),
+                    System.Text.Encoding.UTF8,
+                    "application/json"));
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var updatedMovie = JsonSerializer.Deserialize<Movie>(content);
+        }
+        private async Task DeleteResourceShortcut()
+        {
+            var response = await _httpClient.DeleteAsync(
+                "api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+        }
+
     }
 }
